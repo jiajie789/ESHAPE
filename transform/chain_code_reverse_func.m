0d28d47b6a9ffee4b9cbe265ce56c19501afae28
pdc789 <149677556+pdc789@users.noreply.github.com>
2023-11-13 18:00:33 +0800

    Delete transform directory

--- transform/chain_code_reverse_func.m
+++
@@ -1,2 +0,0 @@
-function[chaincode] = chain_code_reverse_func(chaincode)
-chaincode = mod(flipud(chaincode')'+4,8);
--- transform/chain_code_starting_func.m
+++
@@ -1,3 +0,0 @@
-function[starting_chaincode] = chain_code_starting_func(chaincode,point)
-st = mod(point,numel(chaincode));
-starting_chaincode = [chaincode(st+1:numel(chaincode)),chaincode(1:st)];
